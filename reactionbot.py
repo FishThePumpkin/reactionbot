b3e8508cf3e1ef361474b4e1055abd874305dc50
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2019-02-19 23:42:38 +1100

    Update reactionbot.py

--- reactionbot.py
+++ reactionbot.py
@@ -33,7 +33,7 @@ async def on_ready():
 async def on_message(message):
     #rachel
     author = message.author
-    if author.id == "246437474463776769":
+    if author.id == "318366307169075201":
         return await client.add_reaction(message, '❤')
     #chance
     chance = randint(1,15)
