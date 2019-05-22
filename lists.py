b4b2d22801b718cab28bd5888de180c22059b4fc
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2019-05-22 20:21:20 +1000

    Update lists.py

--- lists.py
+++ lists.py
@@ -17,7 +17,7 @@ reactionsdict = {
 inmsg = {
     "wink": "lstwink",
     "baka": "lsttsun",
-    "?": "lstquestion",
+    "123?": "lstquestion",
     "toxic": "lsttoxic",
     "<@547365819122712586>": "lstping",
     ":GWsocksBlobAngeryPing:": "lstdash",
