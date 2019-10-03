497cf906c88223ae806b72924ece68c9ef946f84
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2019-10-03 17:04:11 +1000

    Update reactionbot.py

--- reactionbot.py
+++ reactionbot.py
@@ -122,7 +122,7 @@ async def on_message(message):
         elif chance in range(1,50):
             return await get_reaction(message,"123jessie")       
         
-    if author.id == IDs["Owner"]:
+    if author.id == IDs["Blue"]:
         chance = randint(1,20)
         if chance == 11:
             return await get_reaction(message,"123blue")
